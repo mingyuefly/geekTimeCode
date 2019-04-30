2bc80edb2f6f3f24f3988e288e7348f11d130112
guomingyue <mingyuefly@qq.com>
2019-04-30 11:57:44 +0800

    BFS

--- 31 BFSAndDFS/AdjacencyArrayDAndW/AdjacencyArrayDAndW/main.c
+++ 31 BFSAndDFS/AdjacencyArrayDAndW/AdjacencyArrayDAndW/main.c
@@ -14,50 +14,16 @@
 
 
 #include <stdio.h>
-
-int book[101], sum, n, e[101][101];
-
-void dfs(int cur)
-{
-    if (sum == n) {
-        return;
-    }
-    sum++;
-    printf(" %d ", cur);
-    for (int i = 1; i <= n; i++) {
-        if (e[cur][i] == 1 && book[i] == 0) {
-            book[i] = 1;
-            dfs(i);
-        }
-    }
-    return;
-}
+#include "Dfs.h"
+#include "Bfs.h"
 
 int main(int argc, const char * argv[]) {
     
-    int i, j, m, a, b;
-    printf("请输入：\n");
-    scanf("%d %d", &n, &m);
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= m; j++) {
-            if (i == j) {
-                e[i][j] = 0;
-            } else {
-                e[i][j] = 999999;
-            }
-        }
-    }
-    for (i = 1; i <= m; i++) {
-        printf("请输入顶点:\n");
-        scanf("%d %d", &a, &b);
-        e[a][b] = 1;
-        e[b][a] = 1;
-    }
+    //dfsFunction();
+    //printf("\n");
     
-    sum = 0;
-    book[1] = 1;
-    dfs(1);
-    //getchar(); getchar();
+    bfsFunction();
+    printf("\n");
     
     return 0;
 }
