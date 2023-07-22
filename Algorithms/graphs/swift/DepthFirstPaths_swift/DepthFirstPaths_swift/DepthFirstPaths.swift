19f9c062a5867d819dd1389bfdcf917569ade8bb
gmy <>
2023-07-23 01:04:19 +0800

    depthFirst in swift

--- Algorithms/graphs/swift/DepthFirstPaths_swift/DepthFirstPaths_swift/DepthFirstPaths.swift
+++ Algorithms/graphs/swift/DepthFirstPaths_swift/DepthFirstPaths_swift/DepthFirstPaths.swift
@@ -9,29 +9,74 @@ import Foundation
 
 class DepthFirstPaths {
     private var marked: [Bool]
-    private var count: Int = 0
-    init(marked: [Bool]) {
+    private var edgeTo: [Int] // 从起点到一个顶点的已知路径上的最后一个顶点
+    private var s: Int // 起点
+
+    init(marked: [Bool], edgeTo: [Int], s: Int) {
         self.marked = marked
+        self.edgeTo = edgeTo
+        self.s = s
     }
     convenience init(g: inout Graph, s: Int) {
-        var marked = [Bool](repeating: false, count: g.getV())
-        self.init(marked: marked)
+        let marked = [Bool](repeating: false, count: g.getV())
+        let edgeTo = [Int](repeating: 0, count: g.getV())
+        self.init(marked: marked, edgeTo: edgeTo, s: s)
         dfs(&g, s)
     }
-    func dfs(_ g: inout Graph, _ v: Int) {
+    private func dfs(_ g: inout Graph, _ v: Int) {
         marked[v] = true
-        count += 1
         g.adj[v].forEach { v1 in
             if !marked[v1] {
+                edgeTo[v1] = v
                 dfs(&g, v1)
-                marked[v1] = true
             }
         }
     }
-    func getMarked(_ v: Int) -> Bool {
+    func hasPathsTo(_ v: Int) -> Bool {
         return marked[v]
     }
-    func getCount() -> Int {
-        return count
+    func showEdge() {
+        for i in 0..<edgeTo.count {
+            print("(\(i):\(edgeTo[i]))")
+        }
+    }
+    func pathTo(_ v: Int) -> Stack<Int> {
+        var stack = Stack<Int>()
+        stack.push(v)
+        var w = v
+        while w != s {
+            stack.push(edgeTo[w])
+            w = edgeTo[w]
+        }
+        return stack
     }
 }
+
+
+//class DepthFirstPaths {
+//    private var marked: [Bool]
+//    private var count: Int = 0
+//    init(marked: [Bool]) {
+//        self.marked = marked
+//    }
+//    convenience init(g: inout Graph, s: Int) {
+//        var marked = [Bool](repeating: false, count: g.getV())
+//        self.init(marked: marked)
+//        dfs(&g, s)
+//    }
+//    func dfs(_ g: inout Graph, _ v: Int) {
+//        marked[v] = true
+//        count += 1
+//        g.adj[v].forEach { v1 in
+//            if !marked[v1] {
+//                dfs(&g, v1)
+//            }
+//        }
+//    }
+//    func getMarked(_ v: Int) -> Bool {
+//        return marked[v]
+//    }
+//    func getCount() -> Int {
+//        return count
+//    }
+//}
--- Algorithms/graphs/swift/DepthFirstPaths_swift/DepthFirstPaths_swift/main.swift
+++ Algorithms/graphs/swift/DepthFirstPaths_swift/DepthFirstPaths_swift/main.swift
@@ -9,7 +9,8 @@ import Foundation
 
 print("Hello, World!")
 
-var g = Graph(8)
+var g = Graph(6)
+//var g = Graph(8)
 g.addEdge(0, 2)
 g.addEdge(0, 1)
 g.addEdge(0, 5)
@@ -18,19 +19,37 @@ g.addEdge(2, 3)
 g.addEdge(2, 4)
 g.addEdge(3, 4)
 g.addEdge(3, 5)
-g.addEdge(7, 6)
+//g.addEdge(7, 6)
 
 g.show()
 print(g.getV())
 print(g.getE())
 
+//let depthFirstPaths = DepthFirstPaths(g: &g, s: 0)
+//for i in 0..<g.getV() {
+//    if depthFirstPaths.getMarked(i) {
+//        print("\(i) ", terminator: "")
+//    }
+//}
+//print("")
+
 let depthFirstPaths = DepthFirstPaths(g: &g, s: 0)
 for i in 0..<g.getV() {
-    if depthFirstPaths.getMarked(i) {
-        print("\(i) ", terminator: "")
-    }
+    print("0 has path to \(i) : \(depthFirstPaths.hasPathsTo(i))")
 }
-print("")
+
+//let depthFirstPaths1 = DepthFirstPaths(g: &g, s: 6)
+//for i in 0..<g.getV() {
+//    print("6 has path to \(i) : \(depthFirstPaths1.hasPathsTo(i))")
+//}
+
+depthFirstPaths.showEdge()
+var path = depthFirstPaths.pathTo(5)
+
+while var empty = path.isEmpty(), !empty {
+    print(path.pop())
+}
+
 
 
 print("end")
